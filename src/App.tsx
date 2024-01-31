3160c2d54e2cf82f4edbf1720a5c2b288b8d3a71
isabella <cisabella626@gmail.com>
2024-01-31 17:02:29 -0300

    Adicionando CRUD-categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 
-import Home from './pages/home/Home';
 import Navbar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListaCategorias from './components/categorias/listaCategorias/ListaCategorias';
+import FormularioCategoria from './components/categorias/formularioCategoria/FormularioCategoria';
+import DeletarCategoria from './components/categorias/deletarCategorias/DeletarCategoria';
+
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
+        <BrowserRouter>
+          <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastrarCategoria" element={<FormularioCategoria />} />
+              <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
+              <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
     </>
-);
+  );
 }
 export default App;
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,27 +1,26 @@
-import React from 'react'
-
+import Logo from '../../assets/dinheiro.svg'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
- 
-  
-
-  return (
-    <>
-     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
-
-            <div className='flex gap-4'>
-              <div className='hover:underline'>Postagens</div>
-              <div className='hover:underline'>Temas</div>
-              <div className='hover:underline'>Cadastrar tema</div>
-              <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
+    
+    return (
+        <>
+        <div className='w-full bg-[#282424] text-white flex justify-center py-4'>
+            <div className="container flex justify-between text-lg">
+                <div className='text-2xl font-bold uppercase'> 
+                <Link to="/">
+                    <img src={Logo} alt="" width="50" />
+                </Link>
+                </div>
+                <div className='flex gap-4'>
+                    <Link to='/produtos' className='hover:underline'>Produtos</Link>
+                    <Link to='/categorias' className='hover:underline'>Categorias</Link>
+                    <Link to='/cadastrarCategoria' className='hover:underline'>Cadastrar categoria</Link>
             </div>
-          </div>
-        </div>
-    </>
-  )
-}
+            </div>
+            </div>
+        </>
+    )
+    }
 
-export default Navbar
+    export default Navbar
